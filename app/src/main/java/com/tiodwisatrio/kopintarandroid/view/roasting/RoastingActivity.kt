1d9a25d2acfef547ff2014b092d6bca18bc17542
tiodwisatrio <tiodwisatrio27@gmail.com>
2024-05-27 22:24:52 +0700

    create bottom navigation menu

--- app/src/main/java/com/tiodwisatrio/kopintarandroid/view/roasting/RoastingActivity.kt
+++ app/src/main/java/com/tiodwisatrio/kopintarandroid/view/roasting/RoastingActivity.kt
@@ -1,21 +1,24 @@
-package com.tiodwisatrio.kopintarandroid.view.home
+package com.tiodwisatrio.kopintarandroid.view.roasting
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract.Profile
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tiodwisatrio.kopintarandroid.R
 import com.tiodwisatrio.kopintarandroid.view.hama.HamaActivity
+import com.tiodwisatrio.kopintarandroid.view.home.MainActivity
 import com.tiodwisatrio.kopintarandroid.view.profile.ProfileActivity
-import com.tiodwisatrio.kopintarandroid.view.roasting.RoastingActivity
 
-class MainActivity : AppCompatActivity() {
+class RoastingActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_roasting)
 
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
@@ -23,30 +26,24 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
-                    // Handle home action
+                    startActivity(Intent(this, MainActivity::class.java))
                     true
                 }
                 R.id.navigation_hama -> {
                     startActivity(Intent(this, HamaActivity::class.java))
                     true
                 }
-
-
-
                 R.id.navigation_roasting -> {
-                    startActivity(Intent(this, RoastingActivity::class.java))
                     true
                 }
-
                 R.id.navigation_profile -> {
                     startActivity(Intent(this, ProfileActivity::class.java))
                     true
                 }
                 else -> false
             }
+
         }
-
-        bottomNavigationView.menu.findItem(R.id.navigation_home).isChecked = true
-
+        bottomNavigationView.menu.findItem(R.id.navigation_roasting).isChecked = true
     }
 }
