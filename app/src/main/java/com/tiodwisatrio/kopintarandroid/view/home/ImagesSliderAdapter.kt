b061c452391faf0873aeacf79b483aa9c67c70e1
tiodwisatrio <tiodwisatrio27@gmail.com>
2024-06-10 21:58:30 +0700

    create image slider using viewpager 2 and create history menu

---
+++ app/src/main/java/com/tiodwisatrio/kopintarandroid/view/home/ImagesSliderAdapter.kt
@@ -0,0 +1,35 @@
+package com.tiodwisatrio.kopintarandroid.view.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.tiodwisatrio.kopintarandroid.R
+
+class ImagesSliderAdapter(private val context: Context, private val images: List<Int>) :
+    RecyclerView.Adapter<ImagesSliderAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_image_slider, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageResId = images[position]
+        holder.bind(imageResId)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.imageViewSlider)
+
+        fun bind(imageResId: Int) {
+            imageView.setImageResource(imageResId)
+        }
+    }
+}
--- app/src/main/java/com/tiodwisatrio/kopintarandroid/view/home/MainActivity.kt
+++ app/src/main/java/com/tiodwisatrio/kopintarandroid/view/home/MainActivity.kt
@@ -2,15 +2,19 @@ package com.tiodwisatrio.kopintarandroid.view.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import androidx.core.util.Pair
+import androidx.viewpager2.widget.ViewPager2
 import com.tiodwisatrio.kopintarandroid.R
 import com.tiodwisatrio.kopintarandroid.data.pref.UserPreferences
 import com.tiodwisatrio.kopintarandroid.databinding.ActivityMainBinding
 import com.tiodwisatrio.kopintarandroid.view.hama.HamaActivity
+import com.tiodwisatrio.kopintarandroid.view.history_hama.HistoryRoastingActivity
 import com.tiodwisatrio.kopintarandroid.view.login.LoginActivity
 import com.tiodwisatrio.kopintarandroid.view.profile.ProfileActivity
 import com.tiodwisatrio.kopintarandroid.view.roasting.RoastingActivity
@@ -22,6 +26,11 @@ class MainActivity : AppCompatActivity() {
         MainViewModelFactory(UserPreferences(this))
     }
 
+    private val images = listOf(
+        R.drawable.image1,
+        R.drawable.image2,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,9 +38,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val adapter = ImagesSliderAdapter(this, images)
+        viewPager.adapter = adapter
+
         setupBottomNavigation()
         observeViewModel()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.history_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_history_pest_detection -> {
+                startActivity(Intent(this, HistoryRoastingActivity::class.java))
+                true
+            }
+
+            R.id.menu_history_roasting -> {
+                startActivity(Intent(this, HistoryRoastingActivity::class.java))
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 
     private fun setupBottomNavigation() {
 
